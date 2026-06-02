68b061c782e2a73eb5190d331bef0fd187e1babd
PolarBear <kkamang03@gmail.com>
2026-06-02 10:58:25 +0900

    test

--- src/pages/board/Home.jsx
+++ src/pages/board/Home.jsx
@@ -52,7 +52,7 @@ const Home =()=>{
 
     return (
       <div className="Home">
-       <h1>전체 게시물 LIST - 나는 오늘 쭈글이 모드야......</h1>
+       <h1>전체 게시물 LIST - 나는 오늘 쭈글이 모드야......ㅠ</h1>
         {
           boards.map( (board)=> <BoardItem key={board.id} board={board}/> )
         }   
